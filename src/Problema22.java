2715993b45558cf8b3f5e6b73895787067b321db
andresemiliano1 <andresemiliano1@gmail.com>
2022-10-05 10:33:40 +0200

    Pseudocodigo ¿?

--- src/Problema22.java
+++ src/Problema22.java
@@ -19,4 +19,18 @@ write “sum from 1 to 10:” + sum
 write “product from 1 to 10:” + product
 
  */
+class Example22 {
+    public static void main(String[] argv) {
+        int sum, product;
+        sum = 0;
+        product = 1;
+        for (int i = 1; i <= 10; i++) {
+            sum = sum + i;
+            product = product * i;
+        }
+        System.out.println("Sum from 1 to 10:" + sum);
+                System.out.println("Product from 1 to 10:" +
+                        product);
+    }
+}
 
