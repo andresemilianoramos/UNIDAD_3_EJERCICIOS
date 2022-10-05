0dca1887ef0bba502faa371f38aeb9b68210537a
andresemiliano1 <andresemiliano1@gmail.com>
2022-10-05 10:31:48 +0200

    Pseudocodigo ¿?

---
+++ src/Problema22.java
@@ -0,0 +1,22 @@
+/*
+Exercise for you to make:
+Write the pseudocode using the equivalent While structure.
+
+Diseña un algoritmo que escriba la suma de los 10 primeros
+números naturales y el producto de los 10 primeros números naturales.
+
+con el while---
+
+while
+sum=0; product=1
+i=0;
+
+while i<=10
+mostrar: sum=sum + i
+mostrar: product=product*i
+end
+write “sum from 1 to 10:” + sum
+write “product from 1 to 10:” + product
+
+ */
+
